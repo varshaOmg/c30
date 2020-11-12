878ca2189cbb91765f798228d2efd96d47cbe886
varsha <rajasekaran.g@gmail.com>
2020-11-12 19:54:35 +0530

    chande sttings

--- Slingshot.js
+++ Slingshot.js
@@ -3,7 +3,7 @@ class SlingShot{
         var options = {
             bodyA: bodyA,
             pointB: pointB,
-            stiffness: 0.04,
+            stiffness: 0.6,
             length: 10
         }
         this.sling1 = loadImage('sprites/sling1.png');
@@ -16,6 +16,11 @@ class SlingShot{
 
     fly(){
         this.sling.bodyA = null;
+    }
+     
+    attach(body){
+        this.sling.bodyA = body
+        
     }
 
     display(){
